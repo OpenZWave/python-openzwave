3ee4cb62de70801167be9d18182c24a16a030b47
bibi21000 <bibi21000@localhost>
2012-09-11 01:24:23 +0200

    Update libopenzwave.pyx

--- lib/libopenzwave.pyx
+++ lib/libopenzwave.pyx
@@ -1,26 +1,26 @@
 """
 .. module:: libopenzwave
 
-This file is part of **py-openzwave** project http://code.google.com/p/python-openzwave.
-    :platform: Unix, Windows
+This file is part of **python-openzwave** project http://code.google.com/p/python-openzwave.
+    :platform: Unix, Windows, MacOS X
     :sinopsis: openzwave wrapper
 
-.. moduleauthor:: maartendamen
 .. moduleauthor:: bibi21000 aka Sébastien GALLET <bibi21000@gmail.com>
+.. moduleauthor:: maarten damen
 
 License : GPL(v3)
 
-**py-openzwave** is free software: you can redistribute it and/or modify
+**python-openzwave** is free software: you can redistribute it and/or modify
 it under the terms of the GNU General Public License as published by
 the Free Software Foundation, either version 3 of the License, or
 (at your option) any later version.
 
-**py-openzwave** is distributed in the hope that it will be useful,
+**python-openzwave** is distributed in the hope that it will be useful,
 but WITHOUT ANY WARRANTY; without even the implied warranty of
 MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the
 GNU General Public License for more details.
 You should have received a copy of the GNU General Public License
-along with py-openzwave. If not, see http://www.gnu.org/licenses.
+along with python-openzwave. If not, see http://www.gnu.org/licenses.
 
 """
 
