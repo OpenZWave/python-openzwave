cd7ebe4091c136ddecc8a25b3b4d60ec277fb0e9
bibi21000 <bibi21000@localhost>
2012-09-25 19:44:44 +0200

    Add update.sh.

---
+++ update.sh
@@ -0,0 +1,22 @@
+#!/bin/bash -e
+
+echo "-----------------------------------------------------------------"
+echo "|   Update sources of python-openzwave                          |"
+echo "-----------------------------------------------------------------"
+hg pull https://code.google.com/p/python-openzwave/
+hg update
+
+echo "-----------------------------------------------------------------"
+echo "|   Update sources of openzwave                                 |"
+echo "-----------------------------------------------------------------"
+if [ -d openzwave ] ; then
+	echo "Update openzwave directory"
+	svn update http://open-zwave.googlecode.com/svn/trunk/ openzwave
+else
+	echo "Checkout openzwave directory"
+	svn checkout http://open-zwave.googlecode.com/svn/trunk/ openzwave
+fi
+
+echo "-----------------------------------------------------------------"
+echo "|   Sources updated                                             |"
+echo "-----------------------------------------------------------------"
