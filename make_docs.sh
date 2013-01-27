265a9ad66cf8beffc0cff1001b5e6bcf0c20ca29
bibi21000 <bibi21000@localhost>
2013-01-27 15:13:57 +0100

    Update docs. Close issue 27

--- make_docs.sh
+++ make_docs.sh
@@ -12,13 +12,13 @@ echo "-----------------------------------------------------------------"
 echo "|   Generate txt docs                                           |"
 echo "-----------------------------------------------------------------"
 make text
-cp _build/text/README.txt ../README
-cp _build/text/INSTALL_REPO.txt ../INSTALL_REPO
-cp _build/text/INSTALL_MAN.txt ../INSTALL_MAN
-cp _build/text/INSTALL_ARCH.txt ../INSTALL_ARCH
-cp _build/text/COPYRIGHT.txt ../COPYRIGHT
-cp _build/text/DEVEL.txt ../DEVEL
-cp _build/text/EXAMPLES.txt ../EXAMPLES
+cp _build/text/README.txt ../
+cp _build/text/INSTALL_REPO.txt ../
+cp _build/text/INSTALL_MAN.txt ../
+cp _build/text/INSTALL_ARCH.txt ../
+cp _build/text/COPYRIGHT.txt ../
+cp _build/text/DEVEL.txt ../
+cp _build/text/EXAMPLES.txt ../
 
 echo "-----------------------------------------------------------------"
 echo "|   Generate html docs                                          |"
