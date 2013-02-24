9b6b38b6ae0eeb4e979445dc33526f175bcea9ea
bibi21000 <bibi21000@localhost>
2013-02-24 20:34:32 +0100

    update install.sh

--- install.sh
+++ install.sh
@@ -4,15 +4,15 @@ echo "-----------------------------------------------------------------"
 echo "|   Install python-openzwave  lib                               |"
 echo "-----------------------------------------------------------------"
 python setup-lib.py install --record install.files
-rm -Rf python_openzwave_lib.egg-info >/dev/null 2>&1
 
 echo "-----------------------------------------------------------------"
 echo "|   Install python-openzwave  api                               |"
 echo "-----------------------------------------------------------------"
 python setup-api.py install --record install.tmp
-rm -Rf python_openzwave_api.egg-info >/dev/null 2>&1
 cat install.tmp >>install.files
 rm install.tmp
+rm -Rf python_openzwave_lib.egg-info >/dev/null 2>&1
+rm -Rf python_openzwave_api.egg-info >/dev/null 2>&1
 
 echo "-----------------------------------------------------------------"
 echo "|   Installation done                                           |"
