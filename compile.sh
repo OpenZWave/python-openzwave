4031211fb2cd91a847b1b38c1b7cf01c92a73e59
nvignal <nvignal@localhost>
2014-07-10 23:40:29 +0200

    Fix vers.cpp location, add .hgignore filter.

--- compile.sh
+++ compile.sh
@@ -6,7 +6,7 @@ CLEAN=0
 echo "-----------------------------------------------------------------"
 echo "|   Build openzwave                                             |"
 echo "-----------------------------------------------------------------"
-rm -Rf openzwave/cpp/build/vers.cpp
+rm -Rf openzwave/cpp/src/vers.cpp
 cd openzwave/
 [ $CLEAN -eq 1 ] && make clean
 make
