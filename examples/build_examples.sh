ebcf04e4cd4308834297316b316f4fdd7535b6ca
bibi21000 <bibi21000@gmail.com>
2015-04-07 23:59:03 +0200

    Remove old examples

--- examples/build_examples.sh
+++
@@ -1,9 +0,0 @@
-#!/bin/bash -e
-
-echo "------------------------------------------------------------"
-echo "|   Install openzwave in tmp                               |"
-echo "------------------------------------------------------------"
-cd ..
-python setup-lib.py install --root=build/tmp
-python setup-api.py install --root=build/tmp
-cd examples
