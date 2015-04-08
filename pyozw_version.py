f59b807bb42daeb3e837f48affc9839595093dfa
bibi21000 <bibi21000@gmail.com>
2015-04-08 13:21:34 +0200

    Update version to 0.3.0-alpha2

--- pyozw_version.py
+++ pyozw_version.py
@@ -21,7 +21,7 @@ You should have received a copy of the GNU General Public License
 along with python-openzwave. If not, see http://www.gnu.org/licenses.
 
 """
-pyozw_version = '0.3.0-alpha1'
+pyozw_version = '0.3.0-alpha2'
 
 if __name__ == '__main__':
     print pyozw_version
