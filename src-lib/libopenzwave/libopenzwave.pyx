57b237521a49576468487601a56b352fd99974d2
Joseph Hughes <jhughes@itriagehealth.com>
2015-12-31 23:26:42 -0700

    more string normalization

--- src-lib/libopenzwave/libopenzwave.pyx
+++ src-lib/libopenzwave/libopenzwave.pyx
@@ -28,6 +28,7 @@ along with python-openzwave. If not, see http://www.gnu.org/licenses.
 
 """
 from cython.operator cimport dereference as deref
+from cpython.version cimport PY_MAJOR_VERSION
 from libcpp.map cimport map, pair
 from libcpp cimport bool
 #from libc.stdint cimport bint
@@ -503,6 +504,21 @@ cdef void ctrl_callback(ControllerState _state, ControllerError _error, void* _c
 
 cpdef object driverData():
     cdef DriverData data
+
+cdef unicode _ustring(s):
+    if type(s) is unicode:
+        # fast path for most common case(s)
+        return <unicode>s
+    elif PY_MAJOR_VERSION < 3 and isinstance(s, bytes):
+        # only accept byte strings in Python 2.x, not in Py3
+        return (<bytes>s).decode('ascii')
+    elif isinstance(s, unicode):
+        # an evil cast to <unicode> might work here in some(!) cases,
+        # depending on what the further processing does.  to be safe,
+        # we can always create a copy instead
+        return unicode(s)
+    else:
+        raise TypeError(...)
 
 cdef unicode to_unicode(s):
     if isinstance(s, bytes):
