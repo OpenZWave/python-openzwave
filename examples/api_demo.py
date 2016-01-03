b0af9cc4bf57ba95a27cdfa90eebd9247adbb8ae
Brad Arndt <barndt@gmail.com>
2016-01-03 03:27:33 +0000

    updated rest of examples to be python 3 compatible, pympler still needs py3 support, louie is py3 supported in my fork

--- examples/api_demo.py
+++ examples/api_demo.py
@@ -91,7 +91,7 @@ if network.state<network.STATE_AWAKED:
     print("Network is not awake but continue anyway")
 print("------------------------------------------------------------")
 print("Use openzwave library : {}".format(network.controller.ozw_library_version))
-print("Use python library : {}".format(network.controller.python_library_version)
+print("Use python library : {}".format(network.controller.python_library_version))
 print("Use ZWave library : {}".format(network.controller.library_description))
 print("Network home id : {}".format(network.home_id_str))
 print("Controller node id : {}".format(network.controller.node.node_id))
@@ -99,7 +99,7 @@ print("Controller node version : {}".format(network.controller.node.version))
 print("Nodes in network : {}".format(network.nodes_count))
 print("------------------------------------------------------------")
 print("Waiting for network ready : ")
-print "------------------------------------------------------------"
+print("------------------------------------------------------------")
 for i in range(0,300):
     if network.state>=network.STATE_READY:
         print(" done in {} seconds".format(time_started))
@@ -115,34 +115,33 @@ for i in range(0,300):
         sys.stdout.flush()
         time.sleep(1.0)
 
-print("Memory use : %s Mo" % (resource.getrusage(resource.RUSAGE_SELF).ru_maxrss / 1024.0))
 
+print("Memory use : {} Mo".format( (resource.getrusage(resource.RUSAGE_SELF).ru_maxrss / 1024.0)))
 if not network.is_ready:
-    print "."
-    print "Network is not ready but continue anyway"
+    print(".")
+    print("Network is not ready but continue anyway")
 
 print("------------------------------------------------------------")
-print "Controller capabilities : %s" % network.controller.capabilities
-print "Controller node capabilities : %s" % network.controller.node.capabilities
-print "Nodes in network : %s" % network.nodes_count
-print "Driver statistics : %s" % network.controller.stats
+print("Controller capabilities : {}".format(network.controller.capabilities))
+print("Controller node capabilities : {}".format(network.controller.node.capabilities))
+print("Nodes in network : {}".format(network.nodes_count))
+print("Driver statistics : {}".format(network.controller.stats))
 print("------------------------------------------------------------")
 for node in network.nodes:
-    print
 
     print("------------------------------------------------------------")
-    print "%s - Name : %s" % (network.nodes[node].node_id,network.nodes[node].name)
-    print "%s - Manufacturer name / id : %s / %s" % (network.nodes[node].node_id,network.nodes[node].manufacturer_name, network.nodes[node].manufacturer_id)
-    print "%s - Product name / id / type : %s / %s / %s" % (network.nodes[node].node_id,network.nodes[node].product_name, network.nodes[node].product_id, network.nodes[node].product_type)
-    print "%s - Version : %s" % (network.nodes[node].node_id, network.nodes[node].version)
-    print "%s - Command classes : %s" % (network.nodes[node].node_id,network.nodes[node].command_classes_as_string)
-    print "%s - Capabilities : %s" % (network.nodes[node].node_id,network.nodes[node].capabilities)
-    print "%s - Neigbors : %s" % (network.nodes[node].node_id,network.nodes[node].neighbors)
-    print "%s - Can sleep : %s" % (network.nodes[node].node_id,network.nodes[node].can_wake_up())
+    print("{} - Name : {}".format(network.nodes[node].node_id,network.nodes[node].name))
+    print("{} - Manufacturer name / id : {} / {}".format(network.nodes[node].node_id,network.nodes[node].manufacturer_name, network.nodes[node].manufacturer_id))
+    print("{} - Product name / id / type : {} / {} / {}".format(network.nodes[node].node_id,network.nodes[node].product_name, network.nodes[node].product_id, network.nodes[node].product_type))
+    print("{} - Version : {}".format(network.nodes[node].node_id, network.nodes[node].version))
+    print("{} - Command classes : {}".format(network.nodes[node].node_id,network.nodes[node].command_classes_as_string))
+    print("{} - Capabilities : {}".format(network.nodes[node].node_id,network.nodes[node].capabilities))
+    print("{} - Neigbors : {}".format(network.nodes[node].node_id,network.nodes[node].neighbors))
+    print("{} - Can sleep : {}".format(network.nodes[node].node_id,network.nodes[node].can_wake_up()))
     groups = {}
     for grp in network.nodes[node].groups :
         groups[network.nodes[node].groups[grp].index] = {'label':network.nodes[node].groups[grp].label, 'associations':network.nodes[node].groups[grp].associations}
-    print "%s - Groups : %s" % (network.nodes[node].node_id, groups)
+    print("{} - Groups : {}".format (network.nodes[node].node_id, groups))
     values = {}
     for val in network.nodes[node].values :
         values[network.nodes[node].values[val].object_id] = {
@@ -155,11 +154,11 @@ for node in network.nodes:
             'data':network.nodes[node].values[val].data_as_string,
             'ispolled':network.nodes[node].values[val].is_polled
             }
-    #print "%s - Values : %s" % (network.nodes[node].node_id, values)
-    #print "------------------------------------------------------------"
+    #print("{} - Values : {}".format(network.nodes[node].node_id, values))
+    #print("------------------------------------------------------------")
     for cmd in network.nodes[node].command_classes:
-        print "   ---------   "
-        #print "cmd = ",cmd
+        print("   ---------   ")
+        #print("cmd = {}".format(cmd)) 
         values = {}
         for val in network.nodes[node].get_values_for_command_class(cmd) :
             values[network.nodes[node].values[val].object_id] = {
@@ -176,108 +175,109 @@ for node in network.nodes:
                 'readonly':network.nodes[node].values[val].is_read_only,
                 'writeonly':network.nodes[node].values[val].is_write_only,
                 }
-        print "%s - Values for command class : %s : %s" % (network.nodes[node].node_id,
+        print("{} - Values for command class : {} : {}".format(network.nodes[node].node_id,
                                     network.nodes[node].get_command_class_as_string(cmd),
-                                    values)
-    print "------------------------------------------------------------"
-print
-print "------------------------------------------------------------"
-print "Driver statistics : %s" % network.controller.stats
-print "------------------------------------------------------------"
-print
-print "------------------------------------------------------------"
-print "Try to autodetect nodes on the network"
-print "------------------------------------------------------------"
-print "Nodes in network : %s" % network.nodes_count
-print "------------------------------------------------------------"
-print "Retrieve switches on the network"
-print "------------------------------------------------------------"
+                                    values))
+    print("------------------------------------------------------------")
+
+print("------------------------------------------------------------")
+print("Driver statistics : {}".format(network.controller.stats))
+print("------------------------------------------------------------")
+
+print("------------------------------------------------------------")
+print("Try to autodetect nodes on the network")
+print("------------------------------------------------------------")
+print("Nodes in network : {}".format(network.nodes_count))
+print("------------------------------------------------------------")
+print("Retrieve switches on the network")
+print("------------------------------------------------------------")
 values = {}
 for node in network.nodes:
     for val in network.nodes[node].get_switches() :
-        print("node/name/index/instance : %s/%s/%s/%s" % (node,network.nodes[node].name,network.nodes[node].values[val].index,network.nodes[node].values[val].instance))
-        print("  label/help : %s/%s" % (network.nodes[node].values[val].label,network.nodes[node].values[val].help))
-        print("  id on the network : %s" % (network.nodes[node].values[val].id_on_network))
-        print("  state: %s" % (network.nodes[node].get_switch_state(val)))
-print "------------------------------------------------------------"
-print "Retrieve dimmers on the network"
-print "------------------------------------------------------------"
+        print("node/name/index/instance : {}/{}/{}/{}".format(node,network.nodes[node].name,network.nodes[node].values[val].index,network.nodes[node].values[val].instance))
+        print("  label/help : {}/{}".format(network.nodes[node].values[val].label,network.nodes[node].values[val].help))
+        print("  id on the network : {}".format(network.nodes[node].values[val].id_on_network))
+        print("  state: {}".format(network.nodes[node].get_switch_state(val)))
+print("------------------------------------------------------------")
+print("Retrieve dimmers on the network")
+print("------------------------------------------------------------")
 values = {}
 for node in network.nodes:
     for val in network.nodes[node].get_dimmers() :
-        print("node/name/index/instance : %s/%s/%s/%s" % (node,network.nodes[node].name,network.nodes[node].values[val].index,network.nodes[node].values[val].instance))
-        print("  label/help : %s/%s" % (network.nodes[node].values[val].label,network.nodes[node].values[val].help))
-        print("  id on the network : %s" % (network.nodes[node].values[val].id_on_network))
-        print("  level: %s" % (network.nodes[node].get_dimmer_level(val)))
-print "------------------------------------------------------------"
-print "Retrieve sensors on the network"
-print "------------------------------------------------------------"
+        print("node/name/index/instance : {}/{}/%s/%s".format (node,network.nodes[node].name,network.nodes[node].values[val].index,network.nodes[node].values[val].instance))
+        print("  label/help : {}/{}".format (network.nodes[node].values[val].label,network.nodes[node].values[val].help))
+        print("  id on the network : {}".format (network.nodes[node].values[val].id_on_network))
+        print("  level: {}".format (network.nodes[node].get_dimmer_level(val)))
+print("------------------------------------------------------------")
+print("Retrieve sensors on the network")
+print("------------------------------------------------------------")
 values = {}
 for node in network.nodes:
     for val in network.nodes[node].get_sensors() :
-        print("node/name/index/instance : %s/%s/%s/%s" % (node,network.nodes[node].name,network.nodes[node].values[val].index,network.nodes[node].values[val].instance))
-        print("  label/help : %s/%s" % (network.nodes[node].values[val].label,network.nodes[node].values[val].help))
-        print("  id on the network : %s" % (network.nodes[node].values[val].id_on_network))
-        print("  value: %s %s" % (network.nodes[node].get_sensor_value(val), network.nodes[node].values[val].units))
-print "------------------------------------------------------------"
-print "Retrieve switches all compatibles devices on the network    "
-print "------------------------------------------------------------"
+        print("node/name/index/instance : {}/{}/{}/{}".format(node,network.nodes[node].name,network.nodes[node].values[val].index,network.nodes[node].values[val].instance))
+        print("  label/help : {}/{}".format(network.nodes[node].values[val].label,network.nodes[node].values[val].help))
+        print("  id on the network : {}".format(network.nodes[node].values[val].id_on_network))
+        print("  value: {} {}".format(network.nodes[node].get_sensor_value(val), network.nodes[node].values[val].units))
+print("------------------------------------------------------------")
+print("Retrieve switches all compatibles devices on the network    ")
+print("------------------------------------------------------------")
 values = {}
 for node in network.nodes:
     for val in network.nodes[node].get_switches_all() :
-        print("node/name/index/instance : %s/%s/%s/%s" % (node,network.nodes[node].name,network.nodes[node].values[val].index,network.nodes[node].values[val].instance))
-        print("  label/help : %s/%s" % (network.nodes[node].values[val].label,network.nodes[node].values[val].help))
-        print("  id on the network : %s" % (network.nodes[node].values[val].id_on_network))
-        print("  value / items: %s / %s" % (network.nodes[node].get_switch_all_item(val), network.nodes[node].get_switch_all_items(val)))
-        print("  state: %s" % (network.nodes[node].get_switch_all_state(val)))
-print "------------------------------------------------------------"
-print "Retrieve protection compatibles devices on the network    "
-print "------------------------------------------------------------"
+        print("node/name/index/instance : {}/{}/{}/{}".format(node,network.nodes[node].name,network.nodes[node].values[val].index,network.nodes[node].values[val].instance))
+        print("  label/help : {}/{}".format(network.nodes[node].values[val].label,network.nodes[node].values[val].help))
+        print("  id on the network : {}".format(network.nodes[node].values[val].id_on_network))
+        print("  value / items:  / {}".format(network.nodes[node].get_switch_all_item(val), network.nodes[node].get_switch_all_items(val)))
+        print("  state: {}".format(network.nodes[node].get_switch_all_state(val)))
+print("------------------------------------------------------------")
+print("------------------------------------------------------------")
+print("Retrieve protection compatibles devices on the network    ")
+print("------------------------------------------------------------")
 values = {}
 for node in network.nodes:
     for val in network.nodes[node].get_protections() :
-        print("node/name/index/instance : %s/%s/%s/%s" % (node,network.nodes[node].name,network.nodes[node].values[val].index,network.nodes[node].values[val].instance))
-        print("  label/help : %s/%s" % (network.nodes[node].values[val].label,network.nodes[node].values[val].help))
-        print("  id on the network : %s" % (network.nodes[node].values[val].id_on_network))
-        print("  value / items: %s / %s" % (network.nodes[node].get_protection_item(val), network.nodes[node].get_protection_items(val)))
-print "------------------------------------------------------------"
+        print("node/name/index/instance : {}/{}/{}/{}".format(node,network.nodes[node].name,network.nodes[node].values[val].index,network.nodes[node].values[val].instance))
+        print("  label/help : {}/{}".format(network.nodes[node].values[val].label,network.nodes[node].values[val].help))
+        print("  id on the network : ".format(network.nodes[node].values[val].id_on_network))
+        print("  value / items: {} / {}".format(network.nodes[node].get_protection_item(val), network.nodes[node].get_protection_items(val)))
+print("------------------------------------------------------------")
 
-print "------------------------------------------------------------"
-print "Retrieve battery compatibles devices on the network         "
-print "------------------------------------------------------------"
+print("------------------------------------------------------------")
+print("Retrieve battery compatibles devices on the network         ")
+print("------------------------------------------------------------")
 values = {}
 for node in network.nodes:
     for val in network.nodes[node].get_battery_levels() :
-        print("node/name/index/instance : %s/%s/%s/%s" % (node,network.nodes[node].name,network.nodes[node].values[val].index,network.nodes[node].values[val].instance))
-        print("  label/help : %s/%s" % (network.nodes[node].values[val].label,network.nodes[node].values[val].help))
-        print("  id on the network : %s" % (network.nodes[node].values[val].id_on_network))
-        print("  value : %s" % (network.nodes[node].get_battery_level(val)))
-print "------------------------------------------------------------"
+        print("node/name/index/instance : {}/{}/{}/{}".format(node,network.nodes[node].name,network.nodes[node].values[val].index,network.nodes[node].values[val].instance))
+        print("  label/help : {}/{}".format(network.nodes[node].values[val].label,network.nodes[node].values[val].help))
+        print("  id on the network : {}".format(network.nodes[node].values[val].id_on_network))
+        print("  value : {}".format(network.nodes[node].get_battery_level(val)))
+print("------------------------------------------------------------")
 
-print "------------------------------------------------------------"
-print "Retrieve power level compatibles devices on the network         "
-print "------------------------------------------------------------"
+print("------------------------------------------------------------")
+print("Retrieve power level compatibles devices on the network         ")
+print("------------------------------------------------------------")
 values = {}
 for node in network.nodes:
     for val in network.nodes[node].get_power_levels() :
-        print("node/name/index/instance : %s/%s/%s/%s" % (node,network.nodes[node].name,network.nodes[node].values[val].index,network.nodes[node].values[val].instance))
-        print("  label/help : %s/%s" % (network.nodes[node].values[val].label,network.nodes[node].values[val].help))
-        print("  id on the network : %s" % (network.nodes[node].values[val].id_on_network))
-        print("  value : %s" % (network.nodes[node].get_power_level(val)))
+        print("node/name/index/instance : {}/{}/{}/{}".format(node,network.nodes[node].name,network.nodes[node].values[val].index,network.nodes[node].values[val].instance))
+        print("  label/help : {}/{}".format(network.nodes[node].values[val].label,network.nodes[node].values[val].help))
+        print("  id on the network : {}".format(network.nodes[node].values[val].id_on_network))
+        print("  value : {}".format(network.nodes[node].get_power_level(val)))
 print("------------------------------------------------------------")
 #print
-#print "------------------------------------------------------------"
+#print("------------------------------------------------------------")
 #print "Activate the switches on the network"
-#print "Nodes in network : %s" % network.nodes_count
-#print "------------------------------------------------------------"
+#print "Nodes in network : {}".format network.nodes_count
+#print("------------------------------------------------------------")
 #for node in network.nodes:
 #    for val in network.nodes[node].get_switches() :
-#        print("Activate switch %s on node %s" % \
+#        print("Activate switch {} on node {}".format \
 #                (network.nodes[node].values[val].label,node))
 #        network.nodes[node].set_switch(val,True)
 #        print("Sleep 10 seconds")
 #        time.sleep(10.0)
-#        print("Dectivate switch %s on node %s" % \
+#        print("Dectivate switch {} on node {}".format \
 #                (network.nodes[node].values[val].label,node))
 #        network.nodes[node].set_switch(val,False)
 #print("Done"))
@@ -292,4 +292,4 @@ print("------------------------------------------------------------")
 print("Stop network")
 print("------------------------------------------------------------")
 network.stop()
-print("Memory use : %s Mo" % (resource.getrusage(resource.RUSAGE_SELF).ru_maxrss / 1024.0))
+print("Memory use : {} Mo".format( (resource.getrusage(resource.RUSAGE_SELF).ru_maxrss / 1024.0)))
