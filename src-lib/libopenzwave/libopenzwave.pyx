71aa055a27be80539a9674dc557a2ddab0a8ce74
ntouran <partofthething@users.noreply.github.com>
2016-01-17 00:40:55 -0800

    Fixed issue where in Python3, all strings were bytes, not strings.
    Also added a unit test to ensure that sensor labels are string types.

--- src-lib/libopenzwave/libopenzwave.pyx
+++ src-lib/libopenzwave/libopenzwave.pyx
@@ -95,7 +95,10 @@ cdef string str_to_cppstr(str s):
 cdef cstr_to_str(s):
     if six.PY3 and not isinstance(s, str):
         return s.decode('utf-8')
-    return s.encode('utf-8')
+    elif six.PY3:
+        return s
+    else:
+        return s.encode('utf-8')
 
 class LibZWaveException(Exception):
     """
