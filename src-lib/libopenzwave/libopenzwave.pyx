76ae0006e6a10da45c31b6bee388e0b72e80699a
Kevin Schlosser <drschlosser@hotmail.com>
2019-01-02 01:13:30 -0700

    adds better handling of strings/bytes/unicode
    currently in the libopenzwave.pyx file there are default data types
    for str that are set for some of the function/method parameters.
    This causes an issue with the test build when running on python 2.
    Because a string literal in python 3 by default is unicode (bytes)
    everything works as advertised. in python 2 string literals are of
    type str(). and because of the marker at the top of the pyx file
    
    #cython: c_string_type=unicode, c_string_encoding=utf8
    
    this is forcing all of the str data types to be unicode.
    Now I could go and write a whole second test routine specifically for
    python 2. But to me this seems like it is not the proper way to go
    about it. Python 2 does not have the best mechanism for dealing with
    unicode strings and it can be very confusing. also because everything
    is defaulted to str() in python 2. I thought it would be best to grab
    the bull by the horns and handle all of the unicode mess internal to
    libopenzwave. This is going to make it much easier for the user to be
    able to use the library with out having to deal with a bunch of
    additional code. especially if their program supports python2 and 3.
    
    It is a real headache to have to decode a path returned by the os module
    for something trivial like a logfile location. and if the user is
    supporting python 2 and 3 it would add a very large amount of code to
    have to deal with issues like this for each and every single
    function/method call to python-openzwave.
    
    So what i did was I removed the str data type from the parameters.
    I created a function that will check the data type based on the
    python version and if it is not either unicode or bytes it will do the
    necessary encoding or decoding to make the data the correct type.
    
    The added benefit of doing this is now all of the tests work
    properly for python 2 without having to recode the tests.
    
    The API has not changed other then now you can pass str, bytes or unicode
    to the functions\methods that would only allow unicode before.
    
    so no changes are made that will affect a users program at all.
    
    the users will be happy. we are happy (no test rewrite to be done).
    seems like a win win to me.
    
    I also took into account setting variables that are of type String as
    well.

--- src-lib/libopenzwave/libopenzwave.pyx
+++ src-lib/libopenzwave/libopenzwave.pyx
@@ -74,6 +74,9 @@ logger.addHandler(NullHandler())
 
 from pkg_resources import get_distribution, DistributionNotFound
 
+
+PY3 = sys.version_info[0] > 2
+
 cdef extern from 'pyversion.h':
     string PY_LIB_VERSION_STRING
     string PY_LIB_FLAVOR_STRING
@@ -659,6 +662,17 @@ def configPath():
                 return os.path.join(libopenzwave_location, PY_OZWAVE_CONFIG_DIRECTORY)
     return None
 
+
+def  convert_string(s):
+    if PY3:
+        if not isinstance(s, bytes):
+            s = s.encode('utf-8')
+    else:
+        if not isinstance(s, unicode):
+            s = s.decode('utf-8')
+    return s
+
+
 cdef class PyOptions:
     """
     Manage options manager
@@ -691,25 +705,27 @@ cdef class PyOptions:
         if os.path.exists(config_path):
             if not os.path.exists(os.path.join(config_path, "zwcfg.xsd")):
                 raise LibZWaveException("Can't retrieve zwcfg.xsd from %s" % config_path)
-            self._config_path = config_path
+
+            self._config_path = convert_string(config_path)
         else:
             raise LibZWaveException("Can't find config directory %s" % config_path)
         if user_path is None:
             user_path = "."
         if os.path.exists(user_path):
             if os.access(user_path, os.W_OK)==True and os.access(user_path, os.R_OK)==True:
-                self._user_path = user_path
+                self._user_path = convert_string(user_path)
             else:
                 raise LibZWaveException("Can't write in user directory %s" % user_path)
         else:
             raise LibZWaveException("Can't find user directory %s" % user_path)
         if cmd_line is None:
             cmd_line=""
-        self._cmd_line = cmd_line
+
+        self._cmd_line = convert_string(cmd_line)
         self.create(self._config_path, self._user_path, self._cmd_line)
 
 
-    def create(self, str a, str b, str c):
+    def create(self, a, b, c):
         """
         .. _createoptions:
 
@@ -725,6 +741,10 @@ cdef class PyOptions:
         :see: destroyoptions_
 
         """
+        a = convert_string(a)
+        b = convert_string(b)
+        c = convert_string(c)
+
         self.options = CreateOptions(
             str_to_cppstr(a), str_to_cppstr(b), str_to_cppstr(c))
         return True
@@ -779,7 +799,7 @@ cdef class PyOptions:
         '''
         return self.options.AreLocked()
 
-    def addOptionBool(self, str name, value):
+    def addOptionBool(self, name, value):
         """
         .. _addOptionBool:
 
@@ -795,9 +815,10 @@ cdef class PyOptions:
         :see: addOption_, addOptionInt_, addOptionString_
 
         """
+        name = convert_string(name)
         return self.options.AddOptionBool(str_to_cppstr(name), value)
 
-    def addOptionInt(self, str name, value):
+    def addOptionInt(self, name, value):
         """
         .. _addOptionInt:
 
@@ -813,9 +834,10 @@ cdef class PyOptions:
         :see: addOption_, addOptionBool_, addOptionString_
 
         """
+        name = convert_string(name)
         return self.options.AddOptionInt(str_to_cppstr(name), value)
 
-    def addOptionString(self, str name, str value, append=False):
+    def addOptionString(self, name, value, append=False):
         """
         .. _addOptionString:
 
@@ -835,6 +857,9 @@ cdef class PyOptions:
         :see: addOption_, addOptionBool_, addOptionInt_
 
         """
+        name = convert_string(name)
+        value = convert_string(value)
+
         return self.options.AddOptionString(
             str_to_cppstr(name), str_to_cppstr(value), append)
 
@@ -854,9 +879,12 @@ cdef class PyOptions:
         :see: addOptionBool_, addOptionInt_, addOptionString_
 
         """
+        name = convert_string(name)
+
         if name not in PyOptionList:
             return False
         if PyOptionList[name]['type'] == "String":
+            value = convert_string(value)
             return self.addOptionString(name, value)
         elif PyOptionList[name]['type'] == "Bool":
             return self.addOptionBool(name, value)
@@ -878,6 +906,7 @@ cdef class PyOptions:
         :see: getOptionAsBool_, getOptionAsInt_, getOptionAsString_
 
         """
+        name = convert_string(name)
         if name not in PyOptionList:
             return None
         if PyOptionList[name]['type'] == "String":
@@ -902,6 +931,7 @@ cdef class PyOptions:
         :see: getOption_, getOptionAsInt_, getOptionAsString_
 
         """
+        name = convert_string(name)
         cdef bool type_bool
         cret = self.options.GetOptionAsBool(str_to_cppstr(name), &type_bool)
         ret = type_bool if cret==True else None
@@ -921,6 +951,7 @@ cdef class PyOptions:
         :see: getOption_, getOptionAsBool_, getOptionAsString_
 
         """
+        name = convert_string(name)
         cdef int32_t type_int
         cret = self.options.GetOptionAsInt(str_to_cppstr(name), &type_int)
         ret = type_int if cret==True else None
@@ -940,6 +971,7 @@ cdef class PyOptions:
         :see: getOption_, getOptionAsBool_, getOptionAsInt_
 
         """
+        name = convert_string(name)
         cdef string type_string
         cret = self.options.GetOptionAsString(str_to_cppstr(name), &type_string)
         ret = cstr_to_str(type_string.c_str()) if cret==True else None
@@ -1210,7 +1242,7 @@ etc.
 # -----------------------------------------------------------------------------
 # Methods for adding and removing drivers and obtaining basic controller information.
 #
-    def addDriver(self, str serialport):
+    def addDriver(self, serialport):
         '''
 .. _addDriver:
 
@@ -1231,9 +1263,11 @@ Home ID is required by most of the OpenZWave Manager class methods.
 :see: removeDriver_
 
         '''
+        serialport = convert_string(serialport)
+
         self.manager.AddDriver(str_to_cppstr(serialport))
 
-    def removeDriver(self, str serialport):
+    def removeDriver(self, serialport):
         '''
 .. _removeDriver:
 
@@ -1249,6 +1283,7 @@ handled automatically.
 :see: addDriver_
 
         '''
+        serialport = convert_string(serialport)
         self.manager.RemoveDriver(str_to_cppstr(serialport))
 
     def getControllerInterfaceType(self, homeid):
@@ -1562,7 +1597,7 @@ Statistics:
 
        '''
         cdef DriverData_t data
-        self.manager.GetDriverStatistics( homeId, &data );
+        self.manager.GetDriverStatistics( homeId, &data )
         ret = {}
         ret['SOFCnt'] = data.m_SOFCnt
         ret['ACKWaiting'] = data.m_ACKWaiting
@@ -2355,7 +2390,7 @@ data.
         cdef string c_string = self.manager.GetNodeProductId(homeid, nodeid)
         return cstr_to_str(c_string.c_str())
 
-    def setNodeManufacturerName(self, homeid, nodeid, str manufacturerName):
+    def setNodeManufacturerName(self, homeid, nodeid, manufacturerName):
         '''
 .. _setNodeManufacturerName:
 
@@ -2378,10 +2413,11 @@ class Value object.
 :see: getNodeManufacturerName_, getNodeProductName_, setNodeProductName_
 
         '''
+        manufacturerName = convert_string(manufacturerName)
         self.manager.SetNodeManufacturerName(
             homeid, nodeid, str_to_cppstr(manufacturerName))
 
-    def setNodeProductName(self, homeid, nodeid, str productName):
+    def setNodeProductName(self, homeid, nodeid, productName):
         '''
 .. _setNodeProductName:
 
@@ -2404,9 +2440,10 @@ class Value object.
 :see: getNodeProductName_, getNodeManufacturerName_, setNodeManufacturerName_
 
         '''
+        productName = convert_string(productName)
         self.manager.SetNodeProductName(homeid, nodeid, str_to_cppstr(productName))
 
-    def setNodeName(self, homeid, nodeid, str name):
+    def setNodeName(self, homeid, nodeid, name):
         '''
 .. _setNodeName:
 
@@ -2429,9 +2466,10 @@ node name is 16 characters.
 :see: getNodeName_, getNodeLocation_, setNodeLocation_
 
         '''
+        name = convert_string(name)
         self.manager.SetNodeName(homeid, nodeid, str_to_cppstr(name))
 
-    def setNodeLocation(self, homeid, nodeid, str location):
+    def setNodeLocation(self, homeid, nodeid, location):
         '''
 .. _setNodeLocation:
 
@@ -2453,6 +2491,7 @@ Node Naming command class, the new location will be sent to the node.
 :see: getNodeLocation_, getNodeName_, setNodeName_
 
         '''
+        location = convert_string(location)
         self.manager.SetNodeLocation(homeid, nodeid, str_to_cppstr(location))
 
     def setNodeOn(self, homeid, nodeid):
@@ -2876,6 +2915,7 @@ if the Z-Wave message actually failed to get through.  Notification callbacks wi
                 cret = self.manager.SetValue(values_map.at(id), type_short)
                 ret = 1 if cret else 0
             elif datatype == "String":
+                value = convert_string(value)
                 if six.PY3:
                     type_string = str_to_cppstr(value)
                 else:
@@ -2932,7 +2972,7 @@ Gets the user-friendly label for the value
         else :
             return None
 
-    def setValueLabel(self, id, str label):
+    def setValueLabel(self, id, label):
         '''
 .. _setValueLabel:
 
@@ -2945,6 +2985,7 @@ Sets the user-friendly label for the value
 :see: getValueLabel_
 
         '''
+        label = convert_string(label)
         if values_map.find(id) != values_map.end():
             self.manager.SetValueLabel(values_map.at(id), str_to_cppstr(label))
 
@@ -2968,7 +3009,7 @@ Gets the units that the value is measured in.
         else :
             return None
 
-    def setValueUnits(self, id, str unit):
+    def setValueUnits(self, id, unit):
         '''
 .. _setValueUnits:
 
@@ -2981,6 +3022,7 @@ Sets the units that the value is measured in.
 :see: getValueUnits_
 
         '''
+        unit = convert_string(unit)
         if values_map.find(id) != values_map.end():
             self.manager.SetValueUnits(values_map.at(id), str_to_cppstr(unit))
 
@@ -3004,7 +3046,7 @@ Gets a help string describing the value's purpose and usage.
         else :
             return None
 
-    def setValueHelp(self, id, str help):
+    def setValueHelp(self, id, help):
         '''
 .. _setValueHelp:
 
@@ -3017,6 +3059,7 @@ Sets a help string describing the value's purpose and usage.
 :see: getValueHelp_
 
         '''
+        help = convert_string(help)
         if values_map.find(id) != values_map.end():
             self.manager.SetValueHelp(values_map.at(id), str_to_cppstr(help))
 
@@ -4775,6 +4818,7 @@ removeSceneValue_, setSceneValue_, sceneGetValues_
                 cret = self.manager.AddSceneValue(sceneid, values_map.at(id), type_short)
                 ret = 1 if cret else 0
             elif datatype == "String":
+                value = convert_string(value)
                 type_string = string(value)
                 cret = self.manager.AddSceneValue(sceneid, values_map.at(id), type_string)
                 ret = 1 if cret else 0
@@ -4863,6 +4907,7 @@ sceneGetValues_
                 cret = self.manager.SetSceneValue(sceneid, values_map.at(id), type_short)
                 ret = 1 if cret else 0
             elif datatype == "String":
+                value = convert_string(value)
                 type_string = string(value)
                 cret = self.manager.SetSceneValue(sceneid, values_map.at(id), type_string)
                 ret = 1 if cret else 0
@@ -4897,7 +4942,7 @@ sceneGetValues_
         cdef string c_string = self.manager.GetSceneLabel(sceneid)
         return cstr_to_str(c_string.c_str())
 
-    def setSceneLabel(self, sceneid, str label):
+    def setSceneLabel(self, sceneid, label):
         '''
 .. _setSceneLabel:
 
@@ -4913,6 +4958,7 @@ getSceneLabel_, removeSceneValue_, addSceneValue_, setSceneValue_, \
 sceneGetValues_
 
         '''
+        label = convert_string(label)
         self.manager.SetSceneLabel(sceneid, str_to_cppstr(label))
 
     def sceneExists(self, sceneid):
