da89112fb6d2311592d9bcd9c227338a93a2d20c
Kevin Schlosser <drschlosser@hotmail.com>
2019-03-24 22:44:55 -0600

    documentation COMMAND_CLASS_POWERLEVEL

--- src-api/openzwave/command_classes/powerlevel.py
+++ src-api/openzwave/command_classes/powerlevel.py
@@ -34,92 +34,209 @@ COMMAND_CLASS_POWERLEVEL = 0x73
 # noinspection PyAbstractClass
 class Powerlevel(CommandClassBase):
 
+    POWER_LEVEL = [
+        'Normal',
+        '-1dB',
+        '-2dB',
+        '-3dB',
+        '-4dB',
+        '-5dB',
+        '-6dB',
+        '-7dB',
+        '-8dB',
+        '-9dB',
+        'Unknown'
+    ]
+
     def __init__(self):
         CommandClassBase.__init__(self)
         self._cls_ids += [COMMAND_CLASS_POWERLEVEL]
 
-    @property
-    def power_level(self):
+    __power_level_doc = """
+        Power Level (`property`)
+        
+        Values:
+        <br></br>
+        * `'Normal'`
+        * `'-1dB'`
+        * `'-2dB'`
+        * `'-3dB'`
+        * `'-4dB'`
+        * `'-5dB'`
+        * `'-6dB'`
+        * `'-7dB'`
+        * `'-8dB'`
+        * `'-9dB'`
+        * `'Unknown'`
+        * `None``
+        
+        :param value: level
+        :type value: int, str
+        :return: level or None if command failed
+        :rtype: str, None
+    """
+
+    def __power_level_get(self):
         key = ('Powerlevel', COMMAND_CLASS_POWERLEVEL)
         try:
             return self[key].data
         except KeyError:
             return None
 
-    @power_level.setter
-    def power_level(self, value):
-        key = ('Powerlevel', COMMAND_CLASS_POWERLEVEL)
-        try:
-            self[key].data = value
-        except KeyError:
-            pass
+    def __power_level_set(self, value):
+        if isinstance(value, int):
+            try:
+                value = self.POWER_LEVEL[value]
+            except IndexError:
+                return
 
-    @property
-    def power_level_timeout(self):
+        if value in self.POWER_LEVEL:
+
+            key = ('Powerlevel', COMMAND_CLASS_POWERLEVEL)
+            try:
+                self[key].data = value
+            except KeyError:
+                pass
+
+    power_level = property(
+        __power_level_get,
+        __power_level_set,
+        doc=__power_level_doc
+    )
+
+    __power_level_timeout_doc = """
+        Power Level Timeout (`property`)
+
+        :param value: timeout
+        :type value: int
+        :return: timeout or None if command failed
+        :rtype: int, None
+    """
+
+    def __power_level_timeout_get(self):
         key = ('Timeout', COMMAND_CLASS_POWERLEVEL)
         try:
             return self[key].data
         except KeyError:
             return None
 
-    @power_level_timeout.setter
-    def power_level_timeout(self, value):
+    def __power_level_timeout_set(self, value):
         key = ('Timeout', COMMAND_CLASS_POWERLEVEL)
         try:
             self[key].data = value
         except KeyError:
             pass
 
-    @property
-    def power_level_test_node(self):
+    power_level_timeout = property(
+        __power_level_timeout_get,
+        __power_level_timeout_set,
+        doc=__power_level_timeout_doc
+    )
+
+    __power_level_test_node_doc = """
+        Power Level Test Node (`property`)
+
+        :param value: node id
+        :type value: int
+        :return: node id or None if command failed
+        :rtype: int, None
+    """
+
+    def __power_level_test_node_get(self):
         key = ('Test Node', COMMAND_CLASS_POWERLEVEL)
         try:
             return self[key].data
         except KeyError:
             return None
 
-    @power_level_test_node.setter
-    def power_level_test_node(self, value):
+    def __power_level_test_node_set(self, value):
         key = ('Test Node', COMMAND_CLASS_POWERLEVEL)
         try:
             self[key].data = value
         except KeyError:
             pass
 
-    @property
-    def power_level_test(self):
+    power_level_test_node = property(
+        __power_level_test_node_get,
+        __power_level_test_node_set,
+        doc=__power_level_test_node_doc
+    )
+
+    __power_level_test_doc = """
+        Power Level Test (`property`)
+        
+        Values:
+        <br></br>
+        * `'Normal'`
+        * `'-1dB'`
+        * `'-2dB'`
+        * `'-3dB'`
+        * `'-4dB'`
+        * `'-5dB'`
+        * `'-6dB'`
+        * `'-7dB'`
+        * `'-8dB'`
+        * `'-9dB'`
+        * `'Unknown'`
+        * `None``
+        
+        :param value: level
+        :type value: int, str
+        :return: level or None if command failed
+        :rtype: str, None
+    """
+
+    def __power_level_test_get(self):
         key = ('Test Powerlevel', COMMAND_CLASS_POWERLEVEL)
         try:
             return self[key].data
         except KeyError:
             return None
 
-    @power_level_test.setter
-    def power_level_test(self, value):
-        key = ('Test Powerlevel', COMMAND_CLASS_POWERLEVEL)
-        try:
-            self[key].data = value
-        except KeyError:
-            pass
+    def __power_level_test_set(self, value):
+
+        if isinstance(value, int):
+            try:
+                value = self.POWER_LEVEL[value]
+            except IndexError:
+                return
+
+        if value in self.POWER_LEVEL:
+
+            key = ('Test Powerlevel', COMMAND_CLASS_POWERLEVEL)
+            try:
+                self[key].data = value
+            except KeyError:
+                pass
+
+    power_level_test = property(
+        __power_level_test_get,
+        __power_level_test_set,
+        doc=__power_level_test_doc
+    )
 
     @property
     def power_level_frame_count(self):
+        """
+        Power Level Frame Count (`property`)
+
+        :return: number of frames or None if command failed
+        :rtype: int, None
+        """
         key = ('Frame Count', COMMAND_CLASS_POWERLEVEL)
         try:
             return self[key].data
         except KeyError:
             return None
 
-    @power_level_frame_count.setter
-    def power_level_frame_count(self, value):
-        key = ('Frame Count', COMMAND_CLASS_POWERLEVEL)
-        try:
-            self[key].data = value
-        except KeyError:
-            pass
-
     @property
     def power_level_acked_frames(self):
+        """
+        Power Level Acknowledged Frames (`property`)
+
+        :return: number of frames or None if command failed
+        :rtype: int, None
+        """
         key = ('Acked Frames', COMMAND_CLASS_POWERLEVEL)
         try:
             return self[key].data
@@ -128,6 +245,19 @@ class Powerlevel(CommandClassBase):
 
     @property
     def power_level_test_results(self):
+        """
+        Power Level Test Results (`property`)
+
+        Values:
+        <br></br>
+        * `'Failed'`
+        * `'Success'`
+        * `'In Progress'`
+        * `'Unknown'`
+
+        :return: test status or None if command failed
+        :rtype: str, None
+        """
         key = ('Test Status', COMMAND_CLASS_POWERLEVEL)
         try:
             return self[key].data
@@ -135,6 +265,12 @@ class Powerlevel(CommandClassBase):
             return None
 
     def power_level_save(self):
+        """
+        Power Level Save
+
+        :return: command successful `True`/`False`
+        :rtype: bool
+        """
         key = ('Set Powerlevel', COMMAND_CLASS_POWERLEVEL)
         try:
             self[key].data = True
@@ -143,6 +279,12 @@ class Powerlevel(CommandClassBase):
             return False
 
     def power_level_start_test(self):
+        """
+        Power Level Start Test
+
+        :return: command successful `True`/`False`
+        :rtype: bool
+        """
         key = ('Test', COMMAND_CLASS_POWERLEVEL)
         try:
             self[key].data = True
@@ -150,7 +292,13 @@ class Powerlevel(CommandClassBase):
         except KeyError:
             return False
 
-    def power_level_reeport(self):
+    def power_level_report(self):
+        """
+        Power Level Report
+
+        :return: command successful `True`/`False`
+        :rtype: bool
+        """
         key = ('Report', COMMAND_CLASS_POWERLEVEL)
         try:
             self[key].data = True
